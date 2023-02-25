130d3ae24a0ad2bb4e4f4300e9b66491ae4b270c
Tayte Boss <boss.tayte@gmail.com>
2023-02-25 13:52:34 +1100

    [feat] Switching layouts to typescript

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,10 +1,19 @@
 import styled from 'styled-components';
 import Header from './Header';
 import Footer from './Footer';
+import { ReactNode } from 'react';
 
 const Main = styled.main``;
 
-const Layout = ({ children }) => {
+type Props = {
+	children: ReactNode;
+};
+
+const Layout = (props: Props) => {
+	const {
+		children
+	} = props;
+
 	return (
 		<>
 			<Header />
